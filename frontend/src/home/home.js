3bfd0ed7bbfc9226fef3db98732090b3df4387e9
Miguel <miguelgandiajorda@gmail.com>
2024-07-02 15:40:15 +0200

    fixing home

--- frontend/src/home/home.js
+++ frontend/src/home/home.js
@@ -1,3 +1,24 @@
 
 
 
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.getElementById('historia').addEventListener('click', function() {
+        console.log("hola historia");
+        window.location.href = '/FURBO/frontend/src/history/history.html';
+    });
+
+    document.getElementById('partidos').addEventListener('click', function() {
+        console.log("hola partidos");
+        window.location.href = '/FURBO/frontend/src/matches/matches.html';
+
+    });
+    
+    document.getElementById('estadio').addEventListener('click', function() {
+        console.log("hola estadio");
+
+    });
+});
