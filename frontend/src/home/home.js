2140cb4f985fa495ddd57b176d59630dc5ea103a
xema <maestrequi@esat-alumni.com>
2024-07-02 23:04:42 +0200

    add new menu

--- frontend/src/home/home.js
+++ frontend/src/home/home.js
@@ -6,7 +6,7 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    document.getElementById('historia').addEventListener('click', function() {
+    /*document.getElementById('historia').addEventListener('click', function() {
         console.log("hola historia");
         window.location.href = '/FURBO/frontend/src/history/history.html';
     });
@@ -21,5 +21,19 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log("hola estadio");
         window.location.href = '/FURBO/frontend/src/stadium/stadium.html';
 
+    });*/
+
+
+    let burger = document.getElementById('navTrigger');
+    let nav  = document.getElementById('navMenu');
+    
+    console.log("Document load");
+
+    console.log(burger);
+    console.log(nav);
+    
+    burger.addEventListener('click', function(e){
+        this.classList.toggle('active');
+        nav.classList.toggle('active');
     });
 });
