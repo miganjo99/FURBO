4a1c2ab86b3c5a53fc3e5ef3c254289604b1a828
xema <maestrequi@esat-alumni.com>
2024-07-17 12:34:36 +0200

    fix menu scroll

--- frontend/src/styles/nav.js
+++ frontend/src/styles/nav.js
@@ -3,5 +3,8 @@ window.addEventListener("scroll", function(){
 
     if(nav.classList.contains("active")){
         this.window.scrollTo(0,0);
+        document.body.style.overflow = 'hidden';
+    }else{
+        document.body.style.overflow = '';
     }
 });
