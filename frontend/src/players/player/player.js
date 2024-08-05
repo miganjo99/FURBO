94040d0f52f27a6372b6218868928090203801b0
Xema <xemaiestacio@gmail.com>
2024-08-05 13:19:28 +0200

    adding player info and e charts

--- frontend/src/players/player/player.js
+++ frontend/src/players/player/player.js
@@ -19,80 +19,237 @@ function GetPlayers() {
   let data = IN_GetPlayer(id);
 
   console.log(data);
-  
+
   document.getElementById("player_name").innerHTML = data.nombre_completo;
-
-  let cardGrid = document.getElementById('player_container');
-
-
-  let chart = echarts.init(document.getElementById("grafo_container"), 'dark',{
-    renderer: 'svg',
-    useDirtyRect: false
-  });
-
-  let max_amarillas_of = IN_GetMaxAmarillasOficial();
-  let max_amarillas = IN_GetMaxAmarillasNo_Oficial();
-  let max_amarillas_total = max_amarillas_of >= max_amarillas ? max_amarillas_of : max_amarillas;
-  console.log("Max amarillas total: " + max_amarillas_total);
+  document.getElementById("player_localidad").innerHTML = data.localidad;
+  document.getElementById("player_fecha_fich").innerHTML = "Fecha fichaje: " + data.fichaje_1;
   
-  let max_rojas_of = IN_GetMaxRojasOficial();
-  let max_rojas = IN_GetMaxRojasNo_Oficial();
-  let max_rojas_total = max_rojas_of >= max_rojas ? max_rojas_of : max_rojas;
-  console.log("Max rojas total: " + max_rojas_total);
+  //if(data.fichaje_2){
+  document.getElementById("player_fecha_fich_2").innerHTML = "Fecha fichaje 2: " + (data.fichaje_2 ? data.fichaje_2 : "Ninguna");
+  //}
+  /*else{
+    document.getElementById("player_fecha_fich_2").innerHTML = "";
+  }*/
 
-  console.log("Max goles: " + IN_GetMaxGoles());
-  console.log("Max asistencias: " + IN_GetMaxAsistencias());
-  console.log("Max convocatorias: " + IN_GetMaxConvocatorias());
-  console.log("Max min jugados: " + IN_GetMaxMinJugados());
-  console.log("Max titular: " + IN_GetMaxTitular());
-  console.log("Max suplente: " + IN_GetMaxSuplente());
 
-let option = {
-  backgroundColor: '#333',
-  title: {
-    //text: 'Basic Radar Chart'
-    text: ''
-  },
-  legend: {
-    data: ['Oficial', 'No oficial']
-  },
-  radar: {
-    shape: 'polygon',
-    indicator: [
-      { name: '% Partidos jugados', max: 100 }, // Percentaje of played matches
-      { name: 'Amarillas', max:  max_amarillas_total}, // Get player with max n of amarillas
-      { name: 'Rojas', max: max_rojas_total }, // Get player with max n of rojas
-      { name: 'Goles', max: IN_GetMaxGoles() }, // Get player with max n of goals
-      { name: 'Asistencias', max: IN_GetMaxAsistencias() }, //
-      { name: 'Convocatorias', max: IN_GetMaxConvocatorias() }, //
-      { name: 'Minutos jugados', max: IN_GetMaxMinJugados() }, //
-      { name: 'Titular', max: IN_GetMaxTitular() }, //
-      { name: 'Suplente', max: IN_GetMaxSuplente() }, //
-    ]
-  },
-  series: [
-    {
-      name: 'Oficial vs No oficial',
-      type: 'radar',
-      data: [
+  document.getElementById("player_fecha_nac").innerHTML = "Fecha nacimiento: " + data.fec_nac;
+  document.getElementById("player_posicion").innerHTML = data.posicion;
+  document.getElementById("player_fecha_sal").innerHTML = "Fecha salida 1: " + data.salida_1;
+  document.getElementById("player_fecha_sal_2").innerHTML = "Fecha salida 2: " + (data.salida_2 ? data.salida_2 : "Ninguna");
+  
+  //document.getElementById("player_name").innerHTML = data.nombre_completo;
+
+// Radar chart
+  {
+
+    
+    let chart = echarts.init(document.getElementById("grafo_container"), 'dark',{
+      renderer: 'svg',
+      useDirtyRect: false
+    });
+    
+    let max_amarillas_of = IN_GetMaxAmarillasOficial();
+    let max_amarillas = IN_GetMaxAmarillasNo_Oficial();
+    let max_amarillas_total = max_amarillas_of >= max_amarillas ? max_amarillas_of : max_amarillas;
+    
+    let max_rojas_of = IN_GetMaxRojasOficial();
+    let max_rojas = IN_GetMaxRojasNo_Oficial();
+    let max_rojas_total = max_rojas_of >= max_rojas ? max_rojas_of : max_rojas;
+    
+    console.log("Max goles: " + IN_GetMaxGoles() + " -> " + data.gol);
+    console.log("Max asistencias: " + IN_GetMaxAsistencias() + " -> " + data.asi);
+    console.log("Max amarillas total: " + max_amarillas_total + " -> " + data.amarillas);
+    console.log("Max rojas total: " + max_rojas_total) + " -> " + data.rojas;
+
+
+    
+    let option = {
+      backgroundColor: '#333',
+      title: {
+        //text: 'Basic Radar Chart'
+        text: ''
+      },
+      legend: {
+        data: ['Oficial', 'No oficial']
+      },
+      radar: {
+        shape: 'polygon',
+        indicator: [
+ 
+          { name: 'Goles', max: IN_GetMaxGoles() }, // Get player with max n of goals
+          { name: 'Asistencias', max: IN_GetMaxAsistencias() }, //
+          { name: 'Amarillas', max:  max_amarillas_total}, // Get player with max n of amarillas
+          { name: 'Rojas', max: max_rojas_total }, // Get player with max n of rojas
+          
+        ]
+      },
+      series: [
         {
-          value: [data.percentaje_pj, data.amarillas, data.rojas, data.gol, data.asi, data.convo, data.min, data.titu, data.sup],
-          name: 'Oficial'
-        },
-        {
-          value: [data.percentaje_pj_of, data.amarilla_of, data.rojas_of, data.gol_of, data.asi_of, data.convo_of, data.min_of, data.titu_of, data.sup_of],
-          name: 'No oficial'
+          name: 'Oficial vs No oficial',
+          type: 'radar',
+          data: [
+            {
+              value: [data.gol, data.asi, data.amarillas, data.rojas],
+              name: 'Oficial'
+            },
+            {
+              value: [data.gol_of, data.asi_of, data.amarilla_of, data.rojas_of],
+              name: 'No oficial'
+            }
+          ]
         }
       ]
-    }
-  ]
-};
+    };
 
 
-  chart.setOption(option);
+    chart.setOption(option);
 
 
-  window.addEventListener('resize', chart.resize);
+    window.addEventListener('resize', chart.resize);
+  }
 
+  // Waterfall chart (convocatorias)
+  {
+    let chart_waterfall = echarts.init(document.getElementById("player_grafo_convocatorias"), 'dark',{
+      renderer: 'svg',
+      useDirtyRect: false
+    });
+
+    option = {
+      backgroundColor: '#333',
+      title: {
+        text: 'Convocatorias',
+        subtext: ''
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow'
+        },
+        formatter: function (params) {
+          var tar = params[1];
+          return tar.name + '<br/>' + tar.seriesName + ' : ' + tar.value;
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: {
+        type: 'category',
+        splitLine: { show: false },
+        data: ['Total', 'Oficial', 'Amistosos']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          name: 'Placeholder',
+          type: 'bar',
+          stack: 'Total',
+          itemStyle: {
+            borderColor: 'transparent',
+            color: 'transparent'
+          },
+          emphasis: {
+            itemStyle: {
+              borderColor: 'transparent',
+              color: 'transparent'
+            }
+          },
+          data: [0, 0, data.convo_of]
+          //data: [0, data.convo - data.convo_of, 0]
+        },
+        {
+          name: 'Convocatorias',
+          type: 'bar',
+          stack: 'Total',
+          label: {
+            show: true,
+            position: 'inside'
+          },
+          data: [data.convo, data.convo_of, data.convo - data.convo_of]
+        }
+      ]
+    };
+
+    chart_waterfall.setOption(option);
+  }
+
+  // Waterfall chart (partidos)
+  {
+    let chart_waterfall = echarts.init(document.getElementById("player_grafo_partidos"), 'dark',{
+      renderer: 'svg',
+      useDirtyRect: false
+    });
+
+    option = {
+      backgroundColor: '#333',
+      title: {
+        text: 'Partidos',
+        subtext: ''
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow'
+        },
+        formatter: function (params) {
+          var tar = params[1];
+          return tar.name + '<br/>' + tar.seriesName + ' : ' + tar.value;
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: {
+        type: 'category',
+        splitLine: { show: false },
+        data: ['Total', 'Oficial', 'Amistosos', 'No jugados']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          name: 'Placeholder',
+          type: 'bar',
+          stack: 'Total',
+          itemStyle: {
+            borderColor: 'transparent',
+            color: 'transparent'
+          },
+          emphasis: {
+            itemStyle: {
+              borderColor: 'transparent',
+              color: 'transparent'
+            }
+          },
+          data: [0, 0, 0, 0]
+          //data: [0, data.convo - data.convo_of, 0]
+        },
+        {
+          name: 'Convocatorias',
+          type: 'bar',
+          stack: 'Total',
+          label: {
+            show: true,
+            position: 'inside'
+          },
+          data: [data.convo, data.pj_of, data.pj - data.pj_of, data.convo - data.pj]
+        }
+      ]
+    };
+
+    chart_waterfall.setOption(option);
+  }
+
+  
 }
 
