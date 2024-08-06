e837f40c5b9610b3ed16092853f12fb157c0b0eb
Xema <xemaiestacio@gmail.com>
2024-08-06 13:30:01 +0200

    add stadium page and google maps link

---
+++ frontend/src/stadium/stadium.js
@@ -0,0 +1,7 @@
+
+window.addEventListener("load", (event) => {
+    console.log("page is fully loaded");
+    document.getElementById("stadium_maps_img").addEventListener("click", function(){
+        window.open("https://www.google.com/maps/place/Estadio+Municipal+El+Clariano/@38.8273406,-0.5964093,15.25z/data=!4m6!3m5!1s0xd619d4e2424626f:0x57e5a482fec83bf1!8m2!3d38.825663!4d-0.5912662!16s%2Fg%2F11bz8flxwc?entry=ttu","_blank");
+    })
+  });
